01707de991b2e56d0e3cff87d6d4f278633c2224
Smit Patel <smitvtridhyatech@gmail.com>
2024-02-27 10:59:29 +0530

    changes in the message of the agency update

--- src/services/chatService.js
+++ src/services/chatService.js
@@ -78,76 +78,10 @@ class ChatService {
           $unwind: { path: "$to_user", preserveNullAndEmptyArrays: true },
         },
       ];
+
       const users = await Chat.aggregate(aggragate).sort({ createdAt: -1 });
 
-      let usersIdString = [];
-      let usersList = [];
-
       // below loop is used to get the unique users list where user had chat
-      for (let i = 0; i < users.length; i++) {
-        let touser = users[i]?.to_user?._id.toString();
-        let fromuser = users[i].fromUser._id.toString();
-
-        if (fromuser == req.user._id && !usersIdString.includes(touser)) {
-          usersIdString.push(touser);
-
-          if (users[i].toUser == null) {
-            continue;
-          }
-          if (users[i].toUser && users[i].toUser.lastName) {
-            users[i].toUser.lastName = users[i].toUser.lastName
-              .charAt(0)
-              .toUpperCase();
-          }
-
-          const unRead = await unreadMesssages.some(
-            (e) =>
-              e.fromUser.toString() == users[i].toUser._id.toString() &&
-              e.toUser.toString() == req.user._id.toString()
-          );
-          if (unRead) {
-            users[i].toUser.unRead = true;
-          } else {
-            users[i].toUser.unRead = false;
-          }
-          users.push(users[i].toUser);
-        } else if (
-          touser == req.user._id &&
-          !usersIdString.includes(fromuser)
-        ) {
-          usersIdString.push(fromuser);
-          // data = await User.findById(users[i].fromUser)
-          //   .select("firstName lastName userName profilePhoto")
-          //   .lean();
-          if (users[i].fromUser == null) {
-            continue;
-          }
-          if (users[i].fromUser && users[i].fromUser.lastName) {
-            users[i].fromUser.lastName = users[i].fromUser.lastName
-              .charAt(0)
-              .toUpperCase();
-          }
-
-          const unRead = await unreadMesssages.some(
-            (e) =>
-              e.fromUser.toString() == users[i].fromUser._id.toString() &&
-              e.toUser.toString() == req.user._id.toString()
-          );
-
-          // const unRead = await Notification.findOne({
-          //   fromUser: users[i].fromUser._id,
-          //   toUser: req.user._id,
-          //   type: "CHATNOTIFICATION",
-          //   read: false,
-          // });
-          if (unRead) {
-            users[i].fromUser.unRead = true;
-          } else {
-            users[i].fromUser.unRead = false;
-          }
-          users.push(users[i].fromUser);
-        }
-      }
     } catch (error) {
       logger.error(`Error while fetching the users list: ${error}`);
       return throwError(error?.message, error?.statusCode);
